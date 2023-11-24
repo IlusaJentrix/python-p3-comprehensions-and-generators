07f5c763dd179dbc9a3d528fd846c5da08775140
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-11-24 12:31:09 +0300

    completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
+
+# Define num_list in the same scope
+num_list = range(1, 10, 2)
+
+# Call the function with num_list
+result = return_evens(num_list)
+print(result)
+  
 
 def make_exclamation(sentence_list):
-    pass
+    return [string + "!" for string in sentence_list]
